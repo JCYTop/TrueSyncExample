0ae02a79bdcf679c0772386d074ca4791c63415f
xiaojinjin <jcyemail@qq.com>
2020-08-13 11:47:25 +0800

    1.搭建初始化测试场景 2.设置初始化脚本

---
+++ Assets/3DSerializer/TruesyncTest.cs
@@ -0,0 +1,35 @@
+﻿using TrueSync;
+using UnityEngine;
+
+/// <summary>
+/// 初始化整个项目使用
+/// </summary>
+public class TruesyncTest : MonoBehaviour
+{
+    public TrueSyncConfig TrueSyncGlobalConfig;
+    private FP lockedTimeStep;
+    private AbstractLockstep lockstep;
+    private CoroutineScheduler scheduler;
+
+    private void Awake()
+    {
+        //直接使用不废话
+        var currentConfig = TrueSyncGlobalConfig;
+        lockedTimeStep = currentConfig.lockedTimeStep;
+        StateTracker.Init(currentConfig.rollbackWindow);
+        TSRandom.Init();
+        if (currentConfig.physics2DEnabled || currentConfig.physics3DEnabled)
+        {
+            PhysicsManager.New(currentConfig);
+            PhysicsManager.instance.LockedTimeStep = lockedTimeStep;
+            PhysicsManager.instance.Init();
+        }
+
+        StateTracker.AddTracking(this, "time");
+    }
+
+    private void Start()
+    {
+        Application.runInBackground = true;
+    }
+}
